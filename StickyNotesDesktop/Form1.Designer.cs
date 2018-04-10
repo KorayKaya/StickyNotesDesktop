0603780ff06276b302f234a2747f9ab77bc36215
KorayKaya <korre.remix@gmail.com>
2018-04-10 14:55:37 +0200

    Upload

---
+++ StickyNotesDesktop/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace StickyNotesDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainTextBox = new System.Windows.Forms.RichTextBox();
+            this.plus_button = new System.Windows.Forms.PictureBox();
+            this.trash_button = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.plus_button)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trash_button)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainTextBox
+            // 
+            this.mainTextBox.AcceptsTab = true;
+            this.mainTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mainTextBox.EnableAutoDragDrop = true;
+            this.mainTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainTextBox.Location = new System.Drawing.Point(12, 60);
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.Size = new System.Drawing.Size(256, 318);
+            this.mainTextBox.TabIndex = 2;
+            this.mainTextBox.Text = "";
+            this.mainTextBox.TextChanged += new System.EventHandler(this.mainTextBox_TextChanged);
+            // 
+            // plus_button
+            // 
+            this.plus_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plus_button.Image = global::StickyNotesDesktop.Properties.Resources.plusButton;
+            this.plus_button.Location = new System.Drawing.Point(178, 12);
+            this.plus_button.Name = "plus_button";
+            this.plus_button.Size = new System.Drawing.Size(42, 42);
+            this.plus_button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.plus_button.TabIndex = 1;
+            this.plus_button.TabStop = false;
+            this.plus_button.Click += new System.EventHandler(this.plus_button_Click);
+            // 
+            // trash_button
+            // 
+            this.trash_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trash_button.Image = global::StickyNotesDesktop.Properties.Resources.trashButton;
+            this.trash_button.Location = new System.Drawing.Point(226, 12);
+            this.trash_button.Name = "trash_button";
+            this.trash_button.Size = new System.Drawing.Size(42, 42);
+            this.trash_button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.trash_button.TabIndex = 0;
+            this.trash_button.TabStop = false;
+            this.trash_button.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trash_button_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(280, 390);
+            this.Controls.Add(this.mainTextBox);
+            this.Controls.Add(this.plus_button);
+            this.Controls.Add(this.trash_button);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(280, 390);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(280, 390);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Sticky Note";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.plus_button)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trash_button)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox trash_button;
+        private System.Windows.Forms.PictureBox plus_button;
+        private System.Windows.Forms.RichTextBox mainTextBox;
+    }
+}
+
---
+++ StickyNotesDesktop/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Linq;
+using System.IO;
+
+namespace StickyNotesDesktop
+{
+    public partial class Form1 : Form
+    {
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+        private int startx, starty;
+        private string mainText;
+        private string savepath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\stickydesktopsave.txt";
+
+        [DllImport("User32.dll")]
+        static extern IntPtr FindWindow(String lpClassName, String lpWindowName);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        [DllImport("user32.dll")]
+        static extern int SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        public Form1(string text = "", int x = -1, int y = -1)
+        {
+            InitializeComponent();
+            mainText = text;
+            mainTextBox.Text = text;
+            startx = x;
+            starty = y;
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var Params = base.CreateParams;
+                Params.ExStyle |= 0x80;
+                return Params;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(save);
+
+            IntPtr pWnd = FindWindow("Progman", "Program Manager");
+            pWnd = FindWindowEx(pWnd, IntPtr.Zero, "SHELLDLL_DefVIew", null);
+            pWnd = FindWindowEx(pWnd, IntPtr.Zero, "SysListView32", null);
+            SetParent(this.Handle, pWnd);
+
+            if (File.Exists(savepath))
+            {
+                string readText = File.ReadAllText(savepath);
+                string[] texts = readText.Split(';');
+                File.Create(savepath).Close();
+                texts = texts.Take(texts.Count() - 1).ToArray();
+
+                if (!readText.Equals(""))
+                {
+                    string s1 = texts[0];
+                    string coordsString = s1.Substring(0, s1.IndexOf(' '));
+                    string[] coords = coordsString.Split(',');
+                    string text = s1.Substring(s1.IndexOf(' ') + 1);
+                    mainTextBox.AppendText(text);
+                    Location= new System.Drawing.Point(Int32.Parse(coords[0]), Int32.Parse(coords[1]));
+
+                    texts = texts.Skip(1).ToArray();
+                    foreach (string s in texts)
+                    {
+                        coordsString = s.Substring(0, s.IndexOf(' '));
+                        coords = coordsString.Split(',');
+                        text = s.Substring(s.IndexOf(' ') + 1);
+                        newNote(text, Int32.Parse(coords[0]), Int32.Parse(coords[1]));
+                    }
+                }
+            }
+
+            if (startx != -1 && starty != -1)
+            {
+                Location = new System.Drawing.Point(startx, starty);
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);
+        }
+
+        private void trash_button_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mainTextBox.Text = "";
+                this.Close();
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private void plus_button_Click(object sender, EventArgs e)
+        {
+            newNote("",-1,-1);
+        }
+
+        private void newNote(string text, int x, int y)
+        {
+            System.Threading.Thread t = new System.Threading.Thread(() => ThreadProc(text, x, y));
+            t.Start();
+        }
+
+        private void save(object sender, EventArgs e)
+        {
+            if (!mainText.Equals(""))
+            {
+                string createText = Location.X + "," + Location.Y + " " +
+                    mainText + ";";
+
+                if (File.Exists(savepath))
+                {
+                    string readText = File.ReadAllText(savepath);
+                    string[] texts = readText.Split(';');
+                    if (!texts.Contains(createText))
+                    {
+                        File.AppendAllText(savepath, createText);
+                    }
+                }
+                else
+                {
+                    File.WriteAllText(savepath, createText);
+                }
+            }
+        }
+
+        private void mainTextBox_TextChanged(object sender, EventArgs e)
+        {
+            mainText = mainTextBox.Text;
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            if (TopMost)
+            {
+                TopLevel = true;
+                TopMost = false;
+            } else
+            {
+                TopMost = true;
+            }
+        }
+
+        public static void ThreadProc(string text,int x, int y)
+        {
+            Application.Run(new Form1(text,x,y));
+        }
+    }
+}
